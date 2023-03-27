ca9715ea110db0d88a5a96036cae55506fda5c3f
N0ku <lucielecouturier2@gmail.com>
2023-03-27 14:48:56 +0200

    📑 - #20 feat/Add connection to the database and getUsers from the DB

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,30 @@
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  axios.get('http://localhost:5050/users')
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  /*   axios.post('http://localhost:5050/users', {
+      email: 'john.doe@example.com',
+      username: 'johndoe',
+      password: 'password123'
+    })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      }); */
+
   return (
     <div className="App">
       <header className="App-header">
--- server/server.js
+++ server/server.js
@@ -1,19 +1,49 @@
-import "./loadEnvironment.mjs";
-
-const express = require("express");
+require('dotenv').config();
 const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+const User = require('./models/user');
+
+
+mongoose.connect(mongoString, { useNewUrlParser: true, useUnifiedTopology: true });
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error);
+});
+
+database.once('open', () => {
+    console.log('Database Connected');
+});
+
 const app = express();
-const http = require("http");
-
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-const PORT = process.env.PORT || 5050;
-const server = http.createServer(app);
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        console.log(users);
+        res.json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
-/* require("")(app, db); */
+app.post('/users', async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username, password });
+        await user.save();
+        res.status(201).json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+app.listen(5050, () => {
+    console.log(`Server Started at ${5050}`);
 });
