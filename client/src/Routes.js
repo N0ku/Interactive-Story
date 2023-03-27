029fe309319afa6eda1599ad2ff8b1e5ebf1af31
N0ku <lucielecouturier2@gmail.com>
2023-03-27 23:40:34 +0200

    #2 🖋️ - feat/finish login and api with getter for informations of the user

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "../src/pages/home/Home";
 import Login from "../src/pages/login/Login";
-import Register from "../src/pages/register/Register"
+import Register from "../src/pages/register/Register";
+import Example from "../src/pages/example/Example";
 
 function RoutesProvider() {
     return (
@@ -12,6 +13,7 @@ function RoutesProvider() {
                 <Route path="/" element={<Home />} />
                 <Route path="/connexion" element={<Login />} />
                 <Route path="/inscription" element={<Register />} />
+                <Route path="/exemple" element={<Example />} />
             </Routes>
         </BrowserRouter>
     );
--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
 const User = require('./models/user');
 const bcrypt = require('bcrypt');
-
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 mongoose.connect(mongoString, { useNewUrlParser: true, useUnifiedTopology: true });
 const database = mongoose.connection;
@@ -22,16 +23,67 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const generateSecretKey = () => {
+    return crypto.randomBytes(32).toString('hex');
+};
+const secretKey = generateSecretKey();
+
+
 app.get('/users', async (req, res) => {
     try {
         const users = await User.find();
-        console.log(users);
         res.json(users);
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
     }
 });
+
+app.post('/authenticate', async (req, res) => {
+    const { identifier, password } = req.body;
+    const user = await User.findOne({ $or: [{ email: identifier }, { username: identifier }] });
+
+    if (!user) {
+        return res.status(404).send("L'utilisateur n'existe pas");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        return res.status(401).send('Mot de passe invalide');
+    }
+
+    const options = { expiresIn: '1h' };
+    const token = jwt.sign({ userId: user._id }, secretKey, options);
+    res.send({ token });
+});
+
+app.get('/protected-resource', async (req, res) => {
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).send('Unauthorized');
+    }
+
+    try {
+        const decoded = jwt.verify(token, secretKey);
+        const userId = decoded.userId;
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        if (Date.now() >= decoded.exp * 1000) {
+            return res.status(401).send('Token expired');
+        }
+        return res.json(user);
+    } catch (err) {
+        console.error(err);
+        return res.status(401).send('Unauthorized');
+    }
+});
+
 
 app.post('/users', async (req, res) => {
     try {
