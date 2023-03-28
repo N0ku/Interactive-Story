fc15919732707573cea006f8cf4f9a6e781bc95d
ThomLam <lamiablethomas@gmail.com>
2023-03-28 04:35:52 +0200

    #12 feature - Try to add Occlusion, Viewer Camera Pos, And another fix in fog.

--- client/src/Kick.js
+++ client/src/Kick.js
@@ -10,12 +10,16 @@ export default function Model(props) {
   const group = useRef()
   const { nodes, materials } = useGLTF('/kick.glb')
 
+  const modelHeight=-1; // Substract the height of the model from the floor
+
   return (
-    <group ref={group} {...props} dispose={null}>
-      <group name="Scene">
-        <group name="Armature" rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
-          <primitive object={nodes.mixamorigHips} />
-          <skinnedMesh name="Ch03" geometry={nodes.Ch03.geometry} material={materials.Ch03_Body} skeleton={nodes.Ch03.skeleton} />
+    <group position={[0, modelHeight, 0]}>
+      <group ref={group} {...props} dispose={null}>
+        <group name="Scene">
+          <group name="Armature" rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+            <primitive object={nodes.mixamorigHips} />
+            <skinnedMesh name="Ch03" geometry={nodes.Ch03.geometry} material={materials.Ch03_Body} skeleton={nodes.Ch03.skeleton} />
+          </group>
         </group>
       </group>
     </group>
