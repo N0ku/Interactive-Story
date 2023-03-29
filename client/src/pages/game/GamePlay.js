134c04d210c2a646e49ba06d90585938ff69b439
Elim Florvil <elim.florvil@icloud.com>
2023-03-29 13:28:12 +0200

    #3 feat

--- client/src/pages/game/GamePlay.js
+++ client/src/pages/game/GamePlay.js
@@ -2,17 +2,11 @@ import React, { useState } from "react";
 import { Canvas } from "react-three-fiber";
 import { OrbitControls, PerspectiveCamera, Plane } from "@react-three/drei";
 import * as THREE from "three";
-import Kick from "../../3dcomponent/Kick.js";
+import Kick from "../../components/Kick.js";
 import InfiniteGround from "../../components/InfiniteGround";
-import Led from "../../3dcomponent/Led.js";
-import Chair from "../../3dcomponent/Chair.js";
-import Baseball from "../../3dcomponent/Baseball.js";
-import ModelLoader from "../../classLoader/ModelLoader";
 
-//TODO 1 -Thomas - 2021-03-28 - Add LOD (Level Of Detail) - Display the appropriate level of detail based on the distance between the camera and the model to reduce the GPU workload.
-//TODO 2 -Thomas - 2021-03-28 - Add Occlusion Frustum - Display the occlusion frustum based on the view camera versus the world, remove if camera don't see him.
-//TODO 3 -Thomas - 2021-03-28 - Modify pixel ratio. Gain 10 fps when using low resolution.
-//TODO 4 -Thomas - 2021-03-28 - Desactivate AntiAliasing. Hard graphic downgrade. gain 20/30 fps when using low resolution.
+
+
 
 function GamePlay() {
   // const [inView, setInView] = useState(false); // Set State false to disable inView.
@@ -43,14 +37,6 @@ function GamePlay() {
           <ambientLight intensity={0.5} />
           <directionalLight intensity={0.5} />
           <InfiniteGround />
-          <Chair />
-          <Led />
-          <ModelLoader
-            path="/low_poly_baseball_bat.glb"
-            position={[0, -1, 1]}
-            rotation={[0, Math.PI / 2, 0]}
-            scale={0.001}
-          />
           <Kick />
           <OrbitControls
             enableDamping
