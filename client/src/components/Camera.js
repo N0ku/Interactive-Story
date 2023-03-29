30c258f62cf8b2e1b9defce53d51522b4226659c
N0ku <lucielecouturier2@gmail.com>
2023-03-29 11:52:29 +0200

    #5 🎥 - feat/add 1st Camera and 3rd person Camera, (hard values only)

--- client/src/components/Camera.js
+++ client/src/components/Camera.js
@@ -1,65 +1,92 @@
 import { OrbitControls, PerspectiveCamera, Plane } from "@react-three/drei";
 import { ReactDOM, useEffect } from "react";
-import React, { useState, Component, useRef} from "react";
+import React, { useState, Component, useRef } from "react";
 import { useFrame } from "react-three-fiber";
 import { Vector3 } from "three";
+import { useControls, button } from 'leva'
+import annotations from './annotations.json'
 
-export default function Camera(props){
-    const { path } = props;
+
+export default function Camera({ lerping, setLerping, path }) {
     const camera = useRef();
     const orbit = useRef();
     const [advance, setAdvance] = useState(true);
-    const [lastPosition, setLastPosition] = useState(null)
-  
-    useFrame((state, delta) => {
-        setLastPosition(path.getPointAt(1));
-        if(advance){
-            const time = state.clock.getElapsedTime();
-            const position = path.getPointAt((time % path.getLength()) / path.getLength());
-            console.log('x: ', position.x)
-            console.log('y: ', position.y)
-            console.log('z: ', Math.trunc(position.z))
-            const tangent = path.getTangentAt((time % path.getLength()) / path.getLength());
-            
-            camera.current.position.set(position.x, position.y, position.z);
-            camera.current.lookAt(new Vector3(position.x + tangent.x, position.y + tangent.y, position.z + tangent.z));
-            if(lastPosition != null){
-                if(lastPosition.z == Math.trunc(position.z)){
-                    setAdvance(false)
-                    orbit.current.target.set(lastPosition.x, 0, lastPosition.z -2 );
-                    orbit.current.update();
-                }
-            }
-           
+    const [lastPosition, setLastPosition] = useState(null);
+    const [to, setTo] = useState(new Vector3(10, 10, 10));
+    const [target, setTarget] = useState(new Vector3(0, 1, 0));
+
+    useControls('Camera', () => {
+        console.log('creating buttons')
+
+        // using reduce
+        const _buttons = annotations.reduce(
+            (acc, a) =>
+                Object.assign(acc, {
+                    [a.title]: button(() => {
+                        setTo(a.position)
+                        setTarget(a.lookAt)
+                        setLerping(true)
+                    })
+                }),
+            {}
+        )
+        return _buttons
+    })
+
+    useFrame(({ camera }, delta) => {
+        if (lerping) {
+            camera.position.lerp(to, delta)
+            orbit.current.target.lerp(target, delta)
         }
-     
-    });
-   return (
-   <>
-   <PerspectiveCamera
-    makeDefault
-    ref={camera} {...props}
-    aspect={window.innerWidth / window.innerHeight}
-            far={500}
+    })
+
+    /*     useFrame((state, delta) => {
+            setLastPosition(path.getPointAt(1));
+            if (advance) {
+                const time = state.clock.getElapsedTime();
+                const position = path.getPointAt((time % path.getLength()) / path.getLength());
+                console.log('x: ', position.x)
+                console.log('y: ', position.y)
+                console.log('z: ', Math.trunc(position.z))
+                const tangent = path.getTangentAt((time % path.getLength()) / path.getLength());
     
+                camera.current.position.set(position.x, position.y, position.z);
+                camera.current.lookAt(new Vector3(position.x + tangent.x, position.y + tangent.y, position.z + tangent.z));
+                if (lastPosition != null) {
+                    if (lastPosition.z == Math.trunc(position.z)) {
+                        setAdvance(false)
+                        orbit.current.target.set(lastPosition.x, 0, lastPosition.z - 2);
+                        orbit.current.update();
+                    }
+                }
     
-    fov={50}
-  />
-  
-  <OrbitControls
-  ref={orbit}
-            enableDamping
-            dampingFactor={0.1}
-            rotateSpeed={0.5} // Speed Rotation
-            minPolarAngle={Math.PI / 6} // Limit angle in down direction
-            maxPolarAngle={Math.PI / 2}
-           
-             // Limit angle in up direction
-          />
-      
-   </>  
-  
-  )
-  ;
-   
+            }
+    
+        }); */
+    return (
+        <>
+            <PerspectiveCamera
+                makeDefault
+                ref={camera} {...path}
+                aspect={window.innerWidth / window.innerHeight}
+                far={500}
+                fov={50}
+            />
+
+            <OrbitControls
+                ref={orbit}
+                enableDamping
+                dampingFactor={0.1}
+                rotateSpeed={0.5} // Speed Rotation
+                minPolarAngle={Math.PI / 6} // Limit angle in down direction
+                maxPolarAngle={Math.PI / 2}
+
+            // Limit angle in up direction
+            />
+
+        </>
+
+    )
+        ;
+
 }
--- client/src/pages/GamePlay.js
+++ client/src/pages/GamePlay.js
@@ -14,15 +14,15 @@ function Ring() {
   const ref = useRef(null);
 
   return (
-     <mesh
-        visible
-        position={[0, 0, 0]}
-        rotation={[190,0, 0]}
-        castShadow
-        ref={ref}>
-           <ringBufferGeometry args={[1, 4, 32]} />
-           <meshBasicMaterial attach="material" color="hotpink" />
-     </mesh>
+    <mesh
+      visible
+      position={[0, 0, 0]}
+      rotation={[190, 0, 0]}
+      castShadow
+      ref={ref}>
+      <ringBufferGeometry args={[1, 4, 32]} />
+      <meshBasicMaterial attach="material" color="hotpink" />
+    </mesh>
   );
 }
 
@@ -31,10 +31,12 @@ function Ring() {
 //TODO 3 -Thomas - 2021-03-28 - Modify pixel ratio. Gain 10 fps when using low resolution.
 //TODO 4 -Thomas - 2021-03-28 - Desactivate AntiAliasing. Hard graphic downgrade. gain 20/30 fps when using low resolution.
 
- function GamePlay() {
+function GamePlay() {
+  const ref = useRef(null);
+  const [lerping, setLerping] = useState(false)
   // const [inView, setInView] = useState(false); // Set State false to disable inView.
   //Fog settings
-  
+
   const fogColor = 0xffffff;
   const fogNear = 5; // Dist to start fog
   const fogFar = 25; // Dist to end fog
@@ -43,47 +45,47 @@ function Ring() {
   const circle = new THREE.Mesh(geometry, material);
 
 scene.add(circle); */
-  
+
 
   const path = new THREE.CatmullRomCurve3([
-  
+
     new THREE.Vector3(0, 0, 22),
-    new THREE.Vector3(0, 0 ,12 ),
-    new THREE.Vector3(0, 2 ,11 ),
-    
+    new THREE.Vector3(0, 0, 12),
+    new THREE.Vector3(0, 2, 11),
 
 
-   
-   
 
-    
+
+
+
+
   ]);
   return (
     <div className="App">
       <div className="canvas-container">
         <Canvas
-          
+
           style={{ width: "100%", height: "100%" }}
           onCreated={({ gl, scene }) => {
             scene.fog = new THREE.Fog(fogColor, fogNear, fogFar);
             gl.setClearColor(scene.fog.color);
           }}
         >
-          <axesHelper scale={[2,2,2]} position={[0,0,0]} />
-         <Camera path={path}></Camera>
+          <axesHelper scale={[2, 2, 2]} position={[0, 0, 0]} />
+          <Camera path={path} lerping={lerping} setLerping={setLerping}></Camera>
           <ambientLight intensity={0.5} />
           <directionalLight intensity={0.5} />
           <InfiniteGround />
-          <Kick position={[0,0,0]}/>
-          <group position={[0,0,10]}>
+          <Kick position={[0, 0, 0]} />
+          <group position={[0, 0, 10]}>
             <Ring></Ring>
           </group>
-          <Kick position={[0,0,10]}/>
-         
-          
-          
-          
-        
+          <Kick position={[0, 0, 10]} />
+
+
+
+
+
         </Canvas>
       </div>
     </div>
