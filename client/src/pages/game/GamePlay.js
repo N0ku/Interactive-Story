e8fd75f71456bdc3dc0cf3bb777aebff8a127fbd
N0ku <lucielecouturier2@gmail.com>
2023-03-30 11:54:56 +0200

    #3 🕺 - feat/Add animations and props for the animations index

--- client/src/pages/game/GamePlay.js
+++ client/src/pages/game/GamePlay.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Canvas, useFrame } from "react-three-fiber";
 import { OrbitControls, PerspectiveCamera, Plane, SpotLight } from "@react-three/drei";
 import * as THREE from "three";
-import Kick from "../../components/Kick.js";
 import InfiniteGround from "../../components/InfiniteGround";
 import BaseballBat from "../../components/BaseballBat.js";
 import Camera from "../../components/Camera"
@@ -10,11 +9,13 @@ import { Sky } from "@react-three/drei";
 import Apocalyptic from "../../components/Apocalyptic.js";
 import { useRef } from "react";
 import DialogueBox from "../../components/DialogueBoxV2";
-
+import Facade from "../../3dcomponent/Strossmayerova_facade.js";
+import StoreAbandonned from "../../3dcomponent/Abandoned_storefront.js";
+import KickAnim from "../../3dcomponent/Kick_anim.js";
 
 function GamePlay() {
-  const batRef = useRef();
   const [lerping, setLerping] = useState(false)
+  const [currentAnimationIndex, setCurrentAnimationIndex] = useState(6);
   // const [inView, setInView] = useState(false); // Set State false to disable inView.
   //Fog settings
 
@@ -22,15 +23,11 @@ function GamePlay() {
   const fogNear = 0; // Dist to start fog
   const fogFar = 2000; // Dist to end fog
 
-  useFrame(() => {
-    const batPosition = batRef.current.position;
-  })
-
   return (
     <div className="App">
       <div className="canvas-container">
         <Canvas
-
+          antialias={false}
           style={{ width: "100%", height: "100%" }}
         // onCreated={({ gl, scene }) => {
         //   scene.fog = new THREE.Fog(fogColor, fogNear, fogFar);
@@ -39,16 +36,25 @@ function GamePlay() {
         >
           <axesHelper scale={[2, 2, 2]} position={[0, 0, 0]} />
 
+          <PerspectiveCamera
+            makeDefault
+            aspect={window.innerWidth / window.innerHeight}
+            far={10000}
+            position={[0, 0, 5]}
+            fov={50}
+          />
           <ambientLight intensity={0.5} />
           <directionalLight intensity={0.5} />
           <group position={[0, -5, 0]}>
             <Apocalyptic />
           </group>
+          {/*    <group>
+            <StoreAbandonned scale={30} position={[0, 0, 0]} rotation={[140 * Math.PI / 180, 0, 0]} />
+          </group> */}
           <group scale={20}>
-            <Kick />
+            <KickAnim animationIndex={currentAnimationIndex} />
           </group>
           <BaseballBat scale={200} position={[619, 10, -800]} rotation={[90 * Math.PI / 180, 340 * Math.PI / 180, 70 * Math.PI / 180]} />
-          <Camera lerping={lerping} setLerping={setLerping}></Camera>
           <OrbitControls
             enableDamping
             dampingFactor={0.1}
@@ -63,7 +69,6 @@ function GamePlay() {
             azimuth={0.25}
           />
         </Canvas>
-        <DialogueBox text="Salut toi ! Comment ça va ? Sartek ton dev brrroo, Askip t'as eu v'la des merdes mais la sa marche :3" speed={25} />
       </div>
     </div>
   );
