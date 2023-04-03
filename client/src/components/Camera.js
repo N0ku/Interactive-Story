9b05b32ceac169e6bc843d8277fecb91fff9b316
Eddy Muzaffar <eddy.muzaffar@outlook.com>
2023-04-03 09:15:06 +0200

    add mouve aand begin rotation b

--- client/src/components/Camera.js
+++ client/src/components/Camera.js
@@ -16,7 +16,7 @@ export default function Camera({ refTargetObject, mode, posRelative, zoom }) {
     const [ifFixed, setFixed] = useState(true);
     const [targetObject, setTargetObject] = useState(null)
     const [positionObj, setPositionObj] = useState([0, 4, -15])
-    
+    const prevPosition = useRef([0, 0, 0]);
 
     var scene = useThree()
 
@@ -37,6 +37,45 @@ export default function Camera({ refTargetObject, mode, posRelative, zoom }) {
         )
         return _buttons
     })
+    const onAxeMove = (position) =>{
+        var dir= ''
+      const x = Math.round(position.x * 100) / 100 ;
+      console.log(position)
+      const y = Math.round(position.y * 100) / 100;
+      
+      const z = Math.round(position.z * 100) / 100;
+      console.log('x', x)
+      console.log('y',y)
+        console.log('z', z)
+        console.log(prevPosition.current[0])
+      // Check if the model is moving along the X-axis or Z-axis
+      if (x != prevPosition.current[0]) {
+        
+        console.log('Model is moving along the X-axis');
+        if (x > prevPosition.current[2]) {
+          
+          dir = 'droite'
+        } else if (x < prevPosition.current[2]) {
+          dir = 'gauche'
+  
+        } else {
+  dir = 'none'
+        }
+      } else if (z != prevPosition.current[2]) {
+        console.log('Model is moving along the Z-axis');
+        if (z > prevPosition.current[2]) {
+  dir = 'devant'
+        } else if (z < prevPosition.current[2]) {
+          dir = 'derriere'
+        } else {
+  dir = 'none'      }    }
+  
+  console.log(dir)
+      // Update the previous position of the model
+      prevPosition.current = [x, y, z];
+      return dir;
+    }
+  
 
     let rotationDuration = 4; // adjust this to control the duration of rotation in seconds
     let rotationTimer = 0;
@@ -48,9 +87,62 @@ export default function Camera({ refTargetObject, mode, posRelative, zoom }) {
         }else{
             modeCamera = mode
         }
-
+        var a = 1
 
         switch(modeCamera){
+            case 'followObjectAbsolu':
+                if(targetObject == null){
+                    setTargetObject(refTargetObject)
+                    return
+                }
+                
+                    
+               
+              
+                if ( ifFixed && refTargetObject.current.position != undefined) {
+
+                    var thirdPersonPosition = posRelative
+                    
+
+                    let position = new THREE.Vector3(0,0,0);
+                    position.setFromMatrixPosition(refTargetObject.current.matrixWorld)
+                    
+                    let quaternion = new THREE.Quaternion(0,0,0,0)
+                    quaternion.setFromRotationMatrix(refTargetObject.current.matrixWorld)
+        
+                    var dir = onAxeMove(position)
+                   var  mirPos = thirdPersonPosition
+                    if(dir == 'devant'){
+                        thirdPersonPosition.x = posRelative.x
+                        thirdPersonPosition.z = posRelative.z
+                            a = 1
+                    }else if(dir =='derriere'){
+                        thirdPersonPosition.x = posRelative.x
+                        thirdPersonPosition.z = posRelative.z
+                            a = -1
+                    }else if(dir == 'gauche'){
+                        thirdPersonPosition.x = mirPos.z
+                        thirdPersonPosition.z = mirPos.x
+                        a = -1
+                    }else{
+                        thirdPersonPosition.x = mirPos.z
+                        thirdPersonPosition.z = mirPos.x
+                        a = 1
+                    }
+                    let wDir =new THREE.Vector3(0,0,-1)
+                    wDir.applyQuaternion(quaternion)
+                    wDir.normalize()
+                    let cameraPos = position.clone().add(
+                        wDir.clone().multiplyScalar(-1).add(
+                            new THREE.Vector3((refTargetObject.current.parent.scale.x * thirdPersonPosition.x) *a,refTargetObject.current.parent.scale.y * thirdPersonPosition.y, (refTargetObject.current.parent.scale.z * thirdPersonPosition.z) * a)
+                        )
+                    ) 
+                    wDir.add(new Vector3(0, 0.2, 0));
+                    camera.position.copy(cameraPos)
+                    camera.lookAt(new THREE.Vector3(position.x, position.y + (refTargetObject.current.parent.scale.y / 2), position.z))
+                
+                }
+                break;
             case 'followObject':
                 if(targetObject == null){
                     setTargetObject(refTargetObject)
@@ -70,11 +162,11 @@ export default function Camera({ refTargetObject, mode, posRelative, zoom }) {
                     quaternion.setFromRotationMatrix(refTargetObject.current.matrixWorld)
         
                     let wDir =new THREE.Vector3(0,0,-1)
-                    
+                    wDir.applyQuaternion(quaternion)
                     wDir.normalize()
                     let cameraPos = position.clone().add(
                         wDir.clone().multiplyScalar(-1).add(
-                            new THREE.Vector3(refTargetObject.current.parent.scale.x * thirdPersonPosition.x,refTargetObject.current.parent.scale.y * thirdPersonPosition.y,refTargetObject.current.parent.scale.z * thirdPersonPosition.z)
+                            new THREE.Vector3(a*(refTargetObject.current.parent.scale.x * thirdPersonPosition.x),refTargetObject.current.parent.scale.y * thirdPersonPosition.y, (refTargetObject.current.parent.scale.z * thirdPersonPosition.z))
                         )
                     ) 
                     wDir.add(new Vector3(0, 0.2, 0));
@@ -122,7 +214,7 @@ export default function Camera({ refTargetObject, mode, posRelative, zoom }) {
                 fov={60}
                 far={800}
                 zoom={zoom}
-                
+                rotation={[Math.PI / 2, 0, 2]}
             >
                 
             </PerspectiveCamera>
--- client/src/components/Kick.js
+++ client/src/components/Kick.js
@@ -9,21 +9,79 @@ function Kick(props) {
   const { nodes, materials } = useGLTF("/kick.glb");
   const [advance, setAdvance] = useState(true)
   const [lastPosition, setLastPosition] = useState();
-  const [difX, setDifX] = useState();
-  const [difZ, setDifZ] = useState();
+  const prevPosition = useRef([0, -0.01, -2]);
+  const rotateQuarternion = useRef(new THREE.Quaternion())
+  const rotateAngle = new THREE.Vector3(0, 1, 0)
   const [rotation, setRotation] = useState( [Math.PI / 2, 0, 0]);
+
 
   
 
   const [positionObj, setPositionObj] = useState([0, -0.01, -2])
   const path = new THREE.CatmullRomCurve3([
     new THREE.Vector3(0, -0.01, -2),
-    new THREE.Vector3(0, 0, 5),
-    new THREE.Vector3(4, 0, 5),
-    new THREE.Vector3(11, 0, 5)
+    new THREE.Vector3(0, 0, 12),
+    new THREE.Vector3(0, 0, 12),
+
+   
   ]);
 
+  const onAxeMove = (position) =>{
+      var dir= ''
+    const x = Math.round(position.x * 100) / 100 ;
+    console.log(position)
+    const y = Math.round(position.y * 100) / 100;
+    
+    const z = Math.round(position.z * 100) / 100;
 
+    // Check if the model is moving along the X-axis or Z-axis
+    if (x !== prevPosition.current[0]) {
+      
+      console.log('Model is moving along the X-axis');
+      if (x > prevPosition.current[2]) {
+        
+        dir = 'droite'
+      } else if (x < prevPosition.current[2]) {
+        dir = 'gauche'
+
+      } else {
+dir = 'none'
+      }
+    } else if (z !== prevPosition.current[2]) {
+      
+      if (z > prevPosition.current[2]) {
+dir = 'devant'
+      } else if (z < prevPosition.current[2]) {
+        dir = 'derriere'
+      } else {
+dir = 'none'      }    }
+console.log(dir)
+
+    // Update the previous position of the model
+    prevPosition.current = [x, y, z];
+    return dir;
+  }
+
+
+  const directionOffset = (dir) => {
+    var dirOff = ''
+
+    if (dir = 'devant') {
+      setRotation([Math.PI / 2, 0, 0])
+      dirOff = Math.PI / 4 // w+a
+    } else if (dir = 'derriere') {
+      setRotation([-Math.PI / 2, 0, 0])
+      dirOff = Math.PI //    
+    } else if (dir = 'gauche') {
+      setRotation([Math.PI / 2, 0, -Math.PI] )   
+    } else if (dir = 'droite') {
+      setRotation([Math.PI / 2, 0, Math.PI] ) 
+      dirOff = - Math.PI / 2 // d
+    }
+
+  
+    return dirOff
+}
   
 
   const modelHeight = -1; // Substract the height of the model from the floor
@@ -33,16 +91,19 @@ function Kick(props) {
      if (advance) {
      
      
-      const t = state.clock.elapsedTime % path.getLength();
-      const position = path.getPoint(t/4);
+      const time = state.clock.elapsedTime % path.getLength();
+      const position = path.getPoint(time/12);
+      /* const time = state.clock.getElapsedTime();
+      const position = path.getPointAt(((time) % path.getLength()) / path.getLength()); */
+  
        
-        
+ 
          setPositionObj([position.x, position.y, position.z])
-         const tangent = path.getTangentAt(((t) % path.getLength()) / path.getLength());
+        /*  const tangent = path.getTangentAt(((t) % path.getLength()) / path.getLength());
          const nextPosition = path.getPoint((t + 0.01) /4 % path.getLength()); // get the next point along the path
-         const angleY = Math.atan2(nextPosition.z - position.z, nextPosition.x - position.x);
+         const angleY = Math.atan2(nextPosition.z - position.z, nextPosition.x - position.x); */
     // Set the Y-axis rotation based on the angle of rotation
-        setRotation([angleY ,  angleY , angleY  ]);
+       
         /*  group.current.rotation.x = tangent.x;
          group.current.rotation.y = tangent.y;
          group.current.rotation.z =tangent.z */
@@ -59,7 +120,7 @@ function Kick(props) {
   };
 
   return (
-    <group ref={group}  onChange={handleChange} position={positionObj}  >
+    <group ref={group}  onChange={handleChange}   position={positionObj}  >
       <group  {...props} dispose={null}  >
         <group name="Scene">
           <group name="Armature" rotation={rotation} scale={0.01} >
