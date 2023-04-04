d760cac574a2818a1f89decbc8b522ebe2c2516d
N0ku <lucielecouturier2@gmail.com>
2023-04-05 00:02:55 +0200

    #28 finish player controls

--- client/src/3dcomponent/Kick_anim.js
+++ client/src/3dcomponent/Kick_anim.js
@@ -1,8 +1,41 @@
 import React, { useRef, useState, useEffect } from "react";
-import { useGLTF, useAnimations, useCursor } from "@react-three/drei";
+import { useGLTF, useAnimations, useCursor, OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
-import { useFrame } from "react-three-fiber";
+import { useFrame, useThree } from "react-three-fiber";
 import { useControls } from "../components/Controls";
+
+
+const directionnalOffset = ({ forward, backward, left, right }) => {
+  var directionnalOffset = 0;
+
+  if (forward) {
+    if (left) {
+      directionnalOffset = Math.PI / 4
+    } else if (right) {
+      directionnalOffset = -Math.PI / 4
+    }
+  }
+  else if (backward) {
+    if (left) {
+      directionnalOffset = Math.PI / 4 + Math.PI /2
+    } else if (right) {
+      directionnalOffset = -Math.PI / 4 - Math.PI / 2
+    } else {
+      directionnalOffset = Math.PI
+    }
+  } else if (left) {
+    directionnalOffset = Math.PI /2
+  } else if (right) {
+    directionnalOffset = - Math.PI / 2
+  }
+
+  return directionnalOffset;
+}
+
+let walkDirection = new THREE.Vector3();
+let rotateAngle = new THREE.Vector3(0, 1, 0);
+let rotateQuarternion = new THREE.Quaternion();
+let cameraTarget = new THREE.Vector3();
 
 function Kick(props) {
   const group = useRef();
@@ -14,14 +47,27 @@ function Kick(props) {
   const { forward, backward, left, right, shift } = useControls();
 
   const currentAnimation = useRef();
+  const controlRef = useRef();
+  const camera = useThree((state) => state.camera)
+
+  const updateCameraTarget = (moveX, moveZ) => {
+    camera.position.x += moveX;
+    camera.position.z += moveZ;
+
+    cameraTarget.x = group.current.position.x;
+    cameraTarget.y = group.current.position.y + 2;
+    cameraTarget.z = group.current.position.z;
+
+    if (controlRef.current) controlRef.current.target = cameraTarget;
+  }
 
   /*   actions[names[props.animationIndex]].reset().fadeIn(0.25).play(); */
   useEffect(() => {
      let action;
     if (forward || backward || left || right) {
-      action = "kick_walk"
+      action = "kick_walkInPlace"
       if (shift) {
-        action = "kick_running"
+        action = "kick_runInPlace"
       }
     }
     else {
@@ -35,25 +81,11 @@ function Kick(props) {
       nextAnimation?.reset().fadeIn(0.2).play();
       currentAnimation.current = action
     } 
-
-    if (group.current) {
-      const box = new THREE.Box3().setFromObject(nodes.Ch03); // Calcal the hard box of model based on skeleton.
-      const center = box.getCenter(new THREE.Vector3());
-      const size = box.getSize(new THREE.Vector3());
-      const hitbox = new THREE.Mesh( // Create the hitbox
-        new THREE.BoxBufferGeometry(size.x, size.y, size.z),
-        new THREE.MeshBasicMaterial({ visible: false })
-      );
-      hitbox.position.copy(center);
-      hitbox.position.y -= (modelHeight - modelHeight); // Shit counter Shit... Center the hitbox in the middle of the model..
-      setBoxHelper(new THREE.BoxHelper(hitbox, 0xff0000)); // Color red because i like red...
-    }
-  }, [props.animationIndex, actions, names, group, modelHeight, nodes.Ch03, forward, backward, left, right, shift]);
+  }, [forward, backward, left, right, shift, actions]);
 
   useCursor(hovered);
 
 
-  var advance = true
 
   const [positionObj, setPositionObj] = useState([0, -0.01, -2])
   const path = new THREE.CatmullRomCurve3([
@@ -68,22 +100,44 @@ function Kick(props) {
   ]);
 
   useFrame((state, delta) => {
+    if (currentAnimation.current === "kick_runInPlace" || currentAnimation.current === "kick_walkInPlace") {
+      let angleYCamera = Math.atan2(
+        camera.position.x - group.current.position.x,
+        camera.position.z - group.current.position.z
+      )
+      let newDirectionOffset = directionnalOffset({
+        forward,
+        backward,
+        left,
+        right
+      });
 
-    /* if (advance) {
-      const time = state.clock.getElapsedTime();
-      const position = path.getPointAt(((time) % path.getLength()) / path.getLength());
-       console.log('x: ', position.x)
-       console.log('y: ', position.y)
-       console.log('z: ', Math.trunc(position.z)) 
-      setPositionObj([position.x, position.y, position.z])
-      props.onSend(group);
-    } */
+      rotateQuarternion.setFromAxisAngle(
+        rotateAngle,
+        angleYCamera + newDirectionOffset
+      );
+      group.current.quaternion.rotateTowards(rotateQuarternion, 0.2)
 
+      camera.getWorldDirection(walkDirection);
+      walkDirection.y = 0;
+      walkDirection.normalize();
+      walkDirection.applyAxisAngle(rotateAngle, newDirectionOffset);
+      
+      const velocity = currentAnimation.current === "kick_runInPlace" ? 10 : 5; // if the player run, velocity to 10 else 5
+
+      const moveX = walkDirection.x * velocity * delta;
+      const moveZ = walkDirection.z * velocity * delta;
+      console.log(walkDirection.x);
+      group.current.position.x += moveX;
+      group.current.position.z += moveZ;
+      updateCameraTarget(moveX, moveZ);
+      console.log(group.current.position);
+    }
   });
 
   return (
-    <group ref={group} position={positionObj}  >
-      <group  {...props} dispose={null}  >
+    <><OrbitControls ref={controlRef} /><group ref={group} position={positionObj}>
+      <group {...props} dispose={null}>
         <group name="Scene">
           <group name="Armature" rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
             <primitive object={nodes.mixamorigHips} />
@@ -91,12 +145,11 @@ function Kick(props) {
               name="Ch03"
               geometry={nodes.Ch03.geometry}
               material={materials.Ch03_Body}
-              skeleton={nodes.Ch03.skeleton}
-            />
+              skeleton={nodes.Ch03.skeleton} />
           </group>
         </group>
       </group>
-    </group>
+    </group></>
   );
 }
 export default Kick;
